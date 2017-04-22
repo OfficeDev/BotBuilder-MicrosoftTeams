3a7d925855609511f0ff8ace00df6cb3d84210ae
Ramjot Singh <ramjsing@microsoft.com>
2017-04-21 17:20:29 -0700

    Removing cards which are not finalized yet.

--- CSharp/Library/Microsoft.Bot.Connector.Teams.Shared/AdditionalProperties.cs
+++ CSharp/Library/Microsoft.Bot.Connector.Teams.Shared/AdditionalProperties.cs
@@ -37,51 +37,51 @@
         public const string ContentType = "viewAction";
     }
 
-    /// <summary>
-    /// Content type for <see cref="PersonCard"/>
-    /// </summary>
-    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Using one file for all additional properties.")]
-    public partial class PersonCard
-    {
-        /// <summary>
-        /// Content type to be used in the type property.
-        /// </summary>
-        public const string ContentType = "application/vnd.microsoft.teams.card.profile";
-    }
+    /////// <summary>
+    /////// Content type for <see cref="PersonCard"/>
+    /////// </summary>
+    ////[SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Using one file for all additional properties.")]
+    ////public partial class PersonCard
+    ////{
+    ////    /// <summary>
+    ////    /// Content type to be used in the type property.
+    ////    /// </summary>
+    ////    public const string ContentType = "application/vnd.microsoft.teams.card.profile";
+    ////}
 
-    /// <summary>
-    /// Content type for <see cref="PersonListItem"/>
-    /// </summary>
-    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Using one file for all additional properties.")]
-    public partial class PersonListItem
-    {
-        /// <summary>
-        /// Content type to be used in the type property.
-        /// </summary>
-        public const string ContentType = "person";
-    }
+    /////// <summary>
+    /////// Content type for <see cref="PersonListItem"/>
+    /////// </summary>
+    ////[SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Using one file for all additional properties.")]
+    ////public partial class PersonListItem
+    ////{
+    ////    /// <summary>
+    ////    /// Content type to be used in the type property.
+    ////    /// </summary>
+    ////    public const string ContentType = "person";
+    ////}
 
-    /// <summary>
-    /// Content type for <see cref="SectionListItem"/>
-    /// </summary>
-    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Using one file for all additional properties.")]
-    public partial class SectionListItem
-    {
-        /// <summary>
-        /// Content type to be used in the type property.
-        /// </summary>
-        public const string ContentType = "section";
-    }
+    /////// <summary>
+    /////// Content type for <see cref="SectionListItem"/>
+    /////// </summary>
+    ////[SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Using one file for all additional properties.")]
+    ////public partial class SectionListItem
+    ////{
+    ////    /// <summary>
+    ////    /// Content type to be used in the type property.
+    ////    /// </summary>
+    ////    public const string ContentType = "section";
+    ////}
 
-    /// <summary>
-    /// Content type for <see cref="ListCard"/>
-    /// </summary>
-    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Using one file for all additional properties.")]
-    public partial class ListCard
-    {
-        /// <summary>
-        /// Content type of List Card
-        /// </summary>
-        public const string ContentType = "application/vnd.microsoft.teams.card.list";
-    }
+    /////// <summary>
+    /////// Content type for <see cref="ListCard"/>
+    /////// </summary>
+    ////[SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Using one file for all additional properties.")]
+    ////public partial class ListCard
+    ////{
+    ////    /// <summary>
+    ////    /// Content type of List Card
+    ////    /// </summary>
+    ////    public const string ContentType = "application/vnd.microsoft.teams.card.list";
+    ////}
 }
