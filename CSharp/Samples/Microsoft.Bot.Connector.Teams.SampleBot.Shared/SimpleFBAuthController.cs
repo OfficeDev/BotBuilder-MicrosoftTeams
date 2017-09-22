0704bb393ed6bbfc944f701cbc6b2f31c38a9a12
Robin Liao <yilia@microsoft.com>
2017-09-21 18:32:19 -0700

    Move SimpleFBAuthController to Microsoft.Bot.Connector.Teams.SampleBot.Shared

--- CSharp/Samples/Microsoft.Bot.Connector.Teams.SampleBot.Shared/SimpleFBAuthController.cs
+++ CSharp/Samples/Microsoft.Bot.Connector.Teams.SampleBot.Shared/SimpleFBAuthController.cs
@@ -33,7 +33,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-namespace Microsoft.Bot.Connector.Teams.SampleBot.Controllers
+namespace Microsoft.Bot.Connector.Teams.SampleBot.Shared
 {
     using System;
     using System.Configuration;
