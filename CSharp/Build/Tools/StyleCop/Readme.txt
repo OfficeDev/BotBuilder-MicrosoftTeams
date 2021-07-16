696c07bd92e16c67e4e0f589bcdd160650fa2eeb
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-16 09:06:33 +0800

    Delete CSharp directory
    Counter terrorism act

--- CSharp/Build/Tools/StyleCop/Readme.txt
+++
@@ -1,6 +0,0 @@
-How-To?
-1) Downloads the package from http://www.nuget.org/packages/StyleCop.MSBuild/
-2) Create a folder for the StyleCop version
-3) Extract contents from .nupkg there (rename the file to .zip)
-4) Optional - Delete all folders except build and tools
-5) Update version of stylecop to use in SkypeTeams.StyleCop.targets
--- CSharp/Library/Microsoft.Bot.Connector.Teams.NetStandard/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1633:File must have header", Justification = "Open source file header is not supported.")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1413:Use trailing comma in multi-line initializers", Justification = "Readability")]
